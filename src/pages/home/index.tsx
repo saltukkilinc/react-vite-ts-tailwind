01d4ab40e80087eebb5746157024b68a89be57b3
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-07-04 20:26:49 +0300

    add crypto

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -9,7 +9,8 @@ const Home = () => {
       <p>Aspernatur soluta voluptatum modi iste consequatur error reiciendis rerum laborum distinctio sequi, fuga ratione. Odio ipsam suscipit iure dolor nihil enim veniam omnis ex dolorum, autem cumque blanditiis quos maiores.</p>
       <p>Eius vitae placeat inventore sint fugit excepturi ut asperiores. Error iure minus distinctio consectetur facilis corrupti repudiandae magnam accusantium impedit ab dolorum, itaque ut omnis voluptatem aperiam earum placeat cumque.</p>
       <p>Repellat officia obcaecati, omnis molestiae, doloremque modi pariatur perferendis laudantium inventore delectus optio tempore soluta nemo maiores, rem explicabo earum! Consequuntur quaerat amet pariatur aspernatur. Atque voluptates consequatur maiores cupiditate.</p>
-      
+      <br />
+      <h1>{self.crypto.randomUUID()}</h1>
     </div>
   )
 }
