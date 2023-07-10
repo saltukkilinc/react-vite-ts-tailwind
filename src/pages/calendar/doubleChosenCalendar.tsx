7a710e6dca81cabf0ac1ce2efa73c3b13cc4c8c1
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-07-10 10:53:47 +0300

    add double Chosen calendar

---
+++ src/pages/calendar/doubleChosenCalendar.tsx
@@ -0,0 +1,42 @@
+import React, { useRef } from 'react'
+import Calendar from 'react-calendar';
+
+type ValuePiece = Date | null;
+
+type Value = ValuePiece | [ValuePiece, ValuePiece];
+
+type propTypes = {
+  value: any,
+  onChangeHandler: (naxtVal: any) => void
+}
+
+const DoubleChosenCalendar = ({value, onChangeHandler}: propTypes) => {
+  const inputRef = useRef();
+ 
+  return (
+    <div>
+       <div className="p-3 grid place-items-center">
+      <Calendar
+        onChange={onChangeHandler}
+        value={value}
+        calendarType="ISO 8601"
+        ref={inputRef}
+        locale="en-US"
+        returnValue="range"
+        selectRange
+      />
+      </div>
+      <div className="mt-8 grid grid-cols-2">
+        <p className="">
+          First Date: {value?.length > 0 && new Date(value[0])?.toLocaleDateString("en-UK")}
+        </p>
+        <p className="">
+          Second Date: {value?.length > 0 && new Date(value[1])?.toLocaleDateString("en-UK")}
+        </p>
+      </div>
+      
+    </div>
+  )
+}
+
+export default DoubleChosenCalendar
--- src/pages/calendar/index.tsx
+++ src/pages/calendar/index.tsx
@@ -1,17 +1,18 @@
 import React, { useState, useRef } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
+import DoubleChosenCalendar from "./doubleChosenCalendar";
 
 type ValuePiece = Date | null;
 
 type Value = ValuePiece | [ValuePiece, ValuePiece];
 
 const CalendarPage = () => {
-  const [value, setValue] = useState<Value | any>();
+  const [value, setValue] = useState<Value>();
   const inputRef = useRef();
 
-  const onChangeHandler = (nextValue: any) => {
-    // const values = nextValue?.map((item: ValuePiece) => new Date(item as Date).toISOString());
+  const onChangeHandler = (nextValue: ValuePiece) => {
+    //const values = nextValue?.map((item: ValuePiece) => new Date(item as Date).toISOString());
     setValue(nextValue);
   };
 
@@ -20,27 +21,9 @@ const CalendarPage = () => {
   // console.log(inputRef);
 
   return (
-    <>
-      <div className="p-3 grid place-items-center">
-      <Calendar
-        onChange={onChangeHandler}
-        value={value as any}
-        calendarType="ISO 8601"
-        ref={inputRef}
-        locale="en-US"
-        returnValue="range"
-        selectRange
-      />
-      </div>
-      <div className="mt-8 grid grid-cols-2">
-        <p className="">
-          First Date: {value?.length > 0 && new Date(value[0])?.toLocaleDateString("en-UK")}
-        </p>
-        <p className="">
-          Second Date: {value?.length > 0 && new Date(value[1])?.toLocaleDateString("en-UK")}
-        </p>
-      </div>
-    </>
+    <div>
+      <DoubleChosenCalendar value={value} onChangeHandler={onChangeHandler} />
+    </div>
   );
 };
 
