ee534f7325611fa1af1ce18174e52a371fca5189
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-07-17 16:57:24 +0300

    add marker

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,15 +1,35 @@
 import React from "react";
 import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
-import 'leaflet/dist/leaflet.css'
+import "leaflet/dist/leaflet.css";
+import L from "leaflet";
 
 const LeafletMap = () => {
+  const selectedPin = new L.Icon({
+    iconUrl: "/pin.png",
+    iconSize: [36, 48],
+    iconAnchor: [36, 0],
+    // popupAnchor: [-3, -40],
+  });
   return (
-    <MapContainer center={[41.0685, 29.2153]} zoom={13} scrollWheelZoom={false} className="w-[700px] h-[400px] mt-10 mx-auto">
+    <MapContainer
+      center={[41.0685, 29.2153]}
+      zoom={13}
+      scrollWheelZoom={false}
+      className="w-[700px] h-[400px] mt-10 mx-auto"
+    >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker
+        position={[41.0685, 29.2153]}
+        key={123}
+        icon={selectedPin}
+        eventHandlers={{
+          click: () => {},
+        }}
+        zIndexOffset={0}
+      >
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
