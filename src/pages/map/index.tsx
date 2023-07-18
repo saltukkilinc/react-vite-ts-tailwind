ce56ff8ab39b7450faee9dcf04c96cc68861247c
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-07-18 10:30:11 +0300

    add map event

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,16 +1,56 @@
-import React from "react";
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import React, { useState } from "react";
+import { MapContainer, TileLayer, useMap, Marker, Popup, useMapEvents } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 
-const LeafletMap = () => {
+
+function LocationMarker() {
+  const defaultPosition = [41.0685, 29.2153];
+  const [position, setPosition] = useState(defaultPosition)
+  const map = useMapEvents({
+    click(e) {
+      map.locate()
+      console.log(e.latlng)
+      setPosition(e?.latlng)
+      map.flyTo(e.latlng, map.getZoom())
+    },
+    // locationfound(e) {
+    //   map.locate()
+    //   setPosition(e.latlng)
+    //   map.flyTo(e.latlng, map.getZoom())
+    // },
+  })
 
   const selectedPin = new L.Icon({
     iconUrl: "/pin.png",
     iconSize: [36, 48],
-    iconAnchor: [36, 0],
+    //iconAnchor: [36, 0],
     // popupAnchor: [-3, -40],
   });
+
+  
+
+  return position === null ? null : (
+    <Marker
+        position={position}
+        key={123}
+        icon={selectedPin}
+        eventHandlers={{
+          click: () => {},
+        }}
+        zIndexOffset={0}
+      >
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+  )
+}
+
+
+const LeafletMap = () => {
+
+ 
   
   return (
     <MapContainer
@@ -23,19 +63,7 @@ const LeafletMap = () => {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker
-        position={[41.0685, 29.2153]}
-        key={123}
-        icon={selectedPin}
-        eventHandlers={{
-          click: () => {},
-        }}
-        zIndexOffset={0}
-      >
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
+      <LocationMarker />
     </MapContainer>
   );
 };
