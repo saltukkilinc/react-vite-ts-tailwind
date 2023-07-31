9952d182a789f9a7e6106c37d2596d0188642b98
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-07-31 09:19:30 +0300

    add FontSorce code

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { store } from "./redux/store";
 import { PDFViewer } from "@react-pdf/renderer";
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/500.css';
+import '@fontsource/poppins/600.css';
+import '@fontsource/poppins/700.css';
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <Provider store={store}>
