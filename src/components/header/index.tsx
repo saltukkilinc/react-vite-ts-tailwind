a427047994a2a3b9290a3850af410e0d5426f021
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-10-09 14:05:40 +0300

    add prettier tailwindcss plugin

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,26 +1,26 @@
-import React from "react";
-import { Link, NavLink } from "react-router-dom";
+import React from 'react';
+import { Link, NavLink } from 'react-router-dom';
 
 const Header = () => {
   const [isDropdownOpen, setIsDropdownOpen] = React.useState<boolean>(false);
 
   return (
-    <header className="min-h-[100px] w-full bg-blue-600 text-white flex flex-col justify-center fixed top-0  gap-3">
-      <ul className="flex justify-around items-center flex-1 shrink-0">
-        <HeaderItem href="home" title="Home" />
-        <HeaderItem href="products" title="Products" />
-        <HeaderItem href="auth" title="Sign-Up/In" />
+    <header className='fixed top-0 flex min-h-[100px] w-full flex-col justify-center gap-3 bg-blue-600  text-white'>
+      <ul className='flex flex-1 shrink-0 items-center justify-around'>
+        <HeaderItem href='home' title='Home' />
+        <HeaderItem href='products' title='Products' />
+        <HeaderItem href='auth' title='Sign-Up/In' />
         <HeaderItem
           title={`${isDropdownOpen ? 'Close' : 'See'} Useful Packages`}
           onClick={() => setIsDropdownOpen(!isDropdownOpen)}
         />
       </ul>
       {isDropdownOpen && (
-        <ul className="flex justify-around items-center flex-1 shrink-0">
-          <HeaderItem href="calendar" title="Calendar" />
-          <HeaderItem href="modal-sheet" title="Modal Sheet" />
-          <HeaderItem href="react-pdf" title="Pdf" />
-          <HeaderItem href="leaflet-map" title="Leaflet Map" />
+        <ul className='flex flex-1 shrink-0 items-center justify-around'>
+          <HeaderItem href='calendar' title='Calendar' />
+          <HeaderItem href='modal-sheet' title='Modal Sheet' />
+          <HeaderItem href='react-pdf' title='Pdf' />
+          <HeaderItem href='leaflet-map' title='Leaflet Map' />
         </ul>
       )}
     </header>
@@ -41,12 +41,10 @@ const HeaderItem = ({ href, title, onClick }: HeaderItemProps) => {
       <NavLink
         onClick={onClick}
         to={href as string}
-        className={({ isActive }) =>
-          isActive ? "text-green-400" : "text-white"
-        }
+        className={({ isActive }) => (isActive ? 'text-green-400' : 'text-white')}
         end
       >
-        <button className="py-1 px-4 bg-blue-500 rounded-lg">{title}</button>
+        <button className='rounded-lg bg-blue-500 px-4 py-1'>{title}</button>
       </NavLink>
     </li>
   );
